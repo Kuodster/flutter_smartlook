1285678d56f4a031ad505968ac7c183ef2534f83
kuodster <kuodster@gmail.com>
2023-08-22 18:00:26 +0500

    * Flutter 3.13.0 support

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/zdenekvanicek/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/zdenekvanicek/Development/smartlook/smartlook-flutter-plugin/flutter_smartlook/example"
+export "FLUTTER_ROOT=/Users/kuodster/Projects/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kuodster/Projects/Flutter-Bambini/flutter_smartlook-4.1.3-flutter-3.13.0/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/zdenekvanicek/Development/smartlook/smartlook-flutter-plugin/flutter_smartlook/example/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/zdenekvanicek/Development/smartlook/smartlook-flutter-plugin/flutter_smartlook/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
--- lib/wireframe/element_descriptors/canvas_descriptor.dart
+++ lib/wireframe/element_descriptors/canvas_descriptor.dart
@@ -1,20 +1,19 @@
 import 'dart:math';
+import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter_smartlook/wireframe/element_data.dart';
 import 'package:flutter_smartlook/wireframe/element_descriptors/utils.dart';
 
-class CustomCanvas extends Canvas {
+class CustomCanvas implements Canvas {
+  final PictureRecorder _recorder;
+  Rect? _cullRect;
   final Function(Rect, Color?) addSkeleton;
 
-  CustomCanvas(
-    PictureRecorder recorder,
-    this.addSkeleton,
-  ) : super(recorder);
+  CustomCanvas(this._recorder, this.addSkeleton, [this._cullRect]);
 
   @override
   void drawRect(Rect rect, Paint paint) async {
-    super.drawRect(rect, paint);
     addSkeleton(rect, paint.color);
     if (paint.shader is Gradient) {
       final Color? color = await getColorFromPaint(paint);
@@ -24,25 +23,22 @@ class CustomCanvas extends Canvas {
 
   @override
   void drawCircle(Offset c, double radius, Paint paint) {
-    super.drawCircle(c, radius, paint);
     addSkeleton(Rect.fromLTWH(c.dx, c.dy, radius, radius), paint.color);
   }
 
   @override
   void drawArc(
-    Rect rect,
-    double startAngle,
-    double sweepAngle,
-    bool useCenter,
-    Paint paint,
-  ) {
-    super.drawArc(rect, startAngle, sweepAngle, useCenter, paint);
+      Rect rect,
+      double startAngle,
+      double sweepAngle,
+      bool useCenter,
+      Paint paint,
+      ) {
     addSkeleton(rect, paint.color);
   }
 
   @override
   void drawPath(Path path, Paint paint) {
-    super.drawPath(path, paint);
     if (paint.style == PaintingStyle.fill) {
       addSkeleton(path.getBounds(), paint.color);
     } else {
@@ -62,7 +58,6 @@ class CustomCanvas extends Canvas {
 
   @override
   void drawLine(Offset p1, Offset p2, Paint paint) {
-    super.drawLine(p1, p2, paint);
     addSkeleton(
       Rect.fromLTWH(
         0,
@@ -121,5 +116,171 @@ class CustomCanvas extends Canvas {
       image.height,
       image.width,
     );
+  }
+
+  @override
+  void clipPath(Path path, {bool doAntiAlias = true}) {
+    // TODO: implement clipPath
+  }
+
+  @override
+  void clipRRect(RRect rrect, {bool doAntiAlias = true}) {
+    // TODO: implement clipRRect
+  }
+
+  @override
+  void clipRect(Rect rect,
+      {ClipOp clipOp = ClipOp.intersect, bool doAntiAlias = true}) {
+    // TODO: implement clipRect
+  }
+
+  @override
+  void drawAtlas(Image atlas, List<RSTransform> transforms, List<Rect> rects,
+      List<Color>? colors, BlendMode? blendMode, Rect? cullRect, Paint paint) {
+    // TODO: implement drawAtlas
+  }
+
+  @override
+  void drawColor(Color color, BlendMode blendMode) {
+    // TODO: implement drawColor
+  }
+
+  @override
+  void drawDRRect(RRect outer, RRect inner, Paint paint) {}
+
+  @override
+  void drawImage(Image image, Offset offset, Paint paint) {
+    // TODO: implement drawImage
+  }
+
+  @override
+  void drawImageNine(Image image, Rect center, Rect dst, Paint paint) {
+    // TODO: implement drawImageNine
+  }
+
+  @override
+  void drawImageRect(Image image, Rect src, Rect dst, Paint paint) {
+    // TODO: implement drawImageRect
+  }
+
+  @override
+  void drawOval(Rect rect, Paint paint) {
+    // TODO: implement drawOval
+  }
+
+  @override
+  void drawPaint(Paint paint) {
+    // TODO: implement drawPaint
+  }
+
+  @override
+  void drawParagraph(Paragraph paragraph, Offset offset) {
+    // TODO: implement drawParagraph
+  }
+
+  @override
+  void drawPicture(Picture picture) {
+    // TODO: implement drawPicture
+  }
+
+  @override
+  void drawPoints(PointMode pointMode, List<Offset> points, Paint paint) {
+    // TODO: implement drawPoints
+  }
+
+  @override
+  void drawRRect(RRect rrect, Paint paint) {
+    // TODO: implement drawRRect
+  }
+
+  @override
+  void drawRawAtlas(Image atlas, Float32List rstTransforms, Float32List rects,
+      Int32List? colors, BlendMode? blendMode, Rect? cullRect, Paint paint) {
+    // TODO: implement drawRawAtlas
+  }
+
+  @override
+  void drawRawPoints(PointMode pointMode, Float32List points, Paint paint) {
+    // TODO: implement drawRawPoints
+  }
+
+  @override
+  void drawShadow(
+      Path path, Color color, double elevation, bool transparentOccluder) {
+    // TODO: implement drawShadow
+  }
+
+  @override
+  void drawVertices(Vertices vertices, BlendMode blendMode, Paint paint) {
+    // TODO: implement drawVertices
+  }
+
+  @override
+  Rect getDestinationClipBounds() {
+    // TODO: implement getDestinationClipBounds
+    throw UnimplementedError();
+  }
+
+  @override
+  Rect getLocalClipBounds() {
+    // TODO: implement getLocalClipBounds
+    throw UnimplementedError();
+  }
+
+  @override
+  int getSaveCount() {
+    // TODO: implement getSaveCount
+    throw UnimplementedError();
+  }
+
+  @override
+  Float64List getTransform() {
+    // TODO: implement getTransform
+    throw UnimplementedError();
+  }
+
+  @override
+  void restore() {
+    // TODO: implement restore
+  }
+
+  @override
+  void restoreToCount(int count) {
+    // TODO: implement restoreToCount
+  }
+
+  @override
+  void rotate(double radians) {
+    // TODO: implement rotate
+  }
+
+  @override
+  void save() {
+    // TODO: implement save
+  }
+
+  @override
+  void saveLayer(Rect? bounds, Paint paint) {
+    // TODO: implement saveLayer
+  }
+
+  @override
+  void scale(double sx, [double? sy]) {
+    // TODO: implement scale
+  }
+
+  @override
+  void skew(double sx, double sy) {
+    // TODO: implement skew
+  }
+
+  @override
+  void transform(Float64List matrix4) {
+    // TODO: implement transform
+  }
+
+  @override
+  void translate(double dx, double dy) {
+    // TODO: implement translate
   }
 }
